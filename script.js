9327afdbb7ecb51c8d8b0821fe0b9eb08f885302
Yashas Shah <79524299+FirewallStampede@users.noreply.github.com>
2021-04-06 09:34:06 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var dark = document.createElement("BUTTON");
+d_theme.innerHTML = "Dark Theme";
+document.body.appendChild(dark);
+d_theme.onclick = function d_dark() {
+
+	document.body.style.background = "#202307";
+
+};
+
+dark.style.width = '55px'; 
+dark.style.height = '55px'; 
+dark.style.background = '#202307'; 
+dark.style.color = 'Snow'; 
+dark.style.fontSize = '15px';
+
